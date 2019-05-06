ea2b0cee32c43886b9c881ef8bca734cb8a9a472
Kelly Mears <developers@tinypixel.dev>
2019-05-06 01:46:05 -0700

    :sparkles: Added activation & deactivation hooks
    Used to flush rewrites based on conditional options flag #1

--- Plugin.php
+++ Plugin.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: Block Sandbox
  * Plugin URI:  https://github.com/kellymears/block-test
@@ -17,11 +18,50 @@ require __DIR__ . '/vendor/autoload.php';
 use \TinyPixel\BlockTools\Sandbox;
 
 /**
- * Run the plugin
- * @hook init
+ * Plugin Main
  */
 add_action('init', function () {
-    (new Sandbox())->create()
-                   ->applyTemplate()
-                   ->register();
+    $sandbox = new Sandbox();
+
+    $sandbox
+        ->create()
+        ->applyTemplate()
+        ->register();
+
+    checkRewriteRules();
 });
+
+/**
+ * Activation hook
+ */
+register_activation_hook(__FILE__, function () {
+    if (!get_option('block_sandbox_flush_rules_flag')) {
+        add_option('block_sandbox_flush_rules_flag', true);
+    }
+});
+
+/**
+ * Deactivation hook
+ */
+register_deactivation_hook(__FILE__, function () {
+    flushRewriteRules();
+});
+
+/**
+ * Check if rewrite rules are set to be flushed
+ */
+function checkRewriteRules()
+{
+    if (get_option('block_sandbox_flush_rules_flag')) {
+        flushRules();
+    }
+}
+
+/**
+ * Flush rewrite rules and remove rewrite flag
+ */
+function flushRules()
+{
+    flush_rewrite_rules();
+    delete_option('block_sandbox_flush_rules_flag');
+}
